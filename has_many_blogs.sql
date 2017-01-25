9becb3fb0b33440cb0558dfca1f325f8a83055dc
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-25 00:05:33 -1000

    refactored code..stopped using double quotes

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -2,13 +2,13 @@
 
 DROP USER IF EXISTS "has_many_user";
 DROP DATABASE IF EXISTS "has_many_blogs";
-CREATE USER "has_many_user";
-CREATE DATABASE "has_many_blogs" WITH OWNER "has_many_user";
+CREATE USER has_many_user;
+CREATE DATABASE has_many_blogs WITH OWNER has_many_user;
 
 \c has_many_blogs;
 
-DROP TABLE IF EXISTS "usesr";
-CREATE TABLE "users"
+DROP TABLE IF EXISTS users;
+CREATE TABLE users
 (
   id serial PRIMARY KEY NOT NULL,
   username varchar(90) NOT NULL,
@@ -18,8 +18,8 @@ CREATE TABLE "users"
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
 
-DROP TABLE IF EXISTS "posts";
-CREATE TABLE "posts"
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts
 (
   id serial PRIMARY KEY NOT NULL,
   title varchar(180),
@@ -27,18 +27,18 @@ CREATE TABLE "posts"
   content text,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  user_id serial NOT NULL REFERENCES users(id)
+  users_id serial NOT NULL REFERENCES users(id)
 );
 
-DROP TABLE IF EXISTS "comments";
-CREATE TABLE "comments"
+DROP TABLE IF EXISTS comments;
+CREATE TABLE comments
 (
   id serial PRIMARY KEY NOT NULL,
   body varchar(510),
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  user_id serial NOT NULL REFERENCES users(id),
-  post_id serial NOT NULL REFERENCES posts(id)
+  users_id serial NOT NULL REFERENCES users(id),
+  posts_id serial NOT NULL REFERENCES posts(id)
 );
 
 \i scripts/blog_data.sql;
--- joins.sql
+++ joins.sql
@@ -1,3 +1,7 @@
-SELECT * FROM "users";
-SELECT * FROM "posts" WHERE "user_id" = 100;
-SELECT * FROM "posts" WHERE
+SELECT * FROM users;
+SELECT * FROM posts WHERE users_id = 100;
+SELECT posts.*, first_name, last_name
+  FROM users
+  INNER JOIN posts
+  ON  posts.users_id = users.id
+  WHERE users_id = 200;
