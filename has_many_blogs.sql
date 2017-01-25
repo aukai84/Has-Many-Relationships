65c08c44d81ae52590e7754d834090fc7d93874c
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-24 21:37:22 -1000

    connecting to db creating tables

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -1,6 +1,11 @@
 \c matthewtirrell
-DROP USER IF EXISTS "has_many_user";
-DROP DATABASE IF EXITS "has_many blogs";
 
+DROP USER IF EXISTS "has_many_user";
+DROP DATABASE IF EXISTS "has_many_blogs";
+CREATE USER "has_many_user";
 CREATE DATABASE "has_many_blogs" WITH OWNER "has_many_user";
 
+\c has_many_blogs;
+
+DROP TABLE IF EXISTS "user";
+--CREATE TABLE "user"
