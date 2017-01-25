ae1e91d08eeb9d5e4846a6e51ad4872b2861db0d
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-24 23:24:22 -1000

    created FK's for my tables... starting on joins.sql

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -10,30 +10,35 @@ CREATE DATABASE "has_many_blogs" WITH OWNER "has_many_user";
 DROP TABLE IF EXISTS "usesr";
 CREATE TABLE "users"
 (
-id serial PRIMARY KEY NOT NULL,
-username varchar(90) NOT NULL,
-first_name varchar(90),
-last_name varchar(90),
-created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+  id serial PRIMARY KEY NOT NULL,
+  username varchar(90) NOT NULL,
+  first_name varchar(90),
+  last_name varchar(90),
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
 
 DROP TABLE IF EXISTS "posts";
 CREATE TABLE "posts"
 (
-id serial PRIMARY KEY NOT NULL,
-title varchar(180),
-url varchar(510),
-content text,
-created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+  id serial PRIMARY KEY NOT NULL,
+  title varchar(180),
+  url varchar(510),
+  content text,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  user_id serial NOT NULL REFERENCES users(id)
 );
 
 DROP TABLE IF EXISTS "comments";
 CREATE TABLE "comments"
 (
-id serial PRIMARY KEY NOT NULL,
-body varchar(510),
-created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+  id serial PRIMARY KEY NOT NULL,
+  body varchar(510),
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  user_id serial NOT NULL REFERENCES users(id),
+  post_id serial NOT NULL REFERENCES posts(id)
 );
+
+\i scripts/blog_data.sql;
---
+++ joins.sql
@@ -0,0 +1,3 @@
+SELECT * FROM "users";
+SELECT * FROM "posts" WHERE "user_id" = 100;
+SELECT * FROM "posts" WHERE
