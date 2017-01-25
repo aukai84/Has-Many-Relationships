d112ef3144fb63056ac3fa2d666ee59c99037798
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-24 22:40:08 -1000

    created users posts and comments tables

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -7,5 +7,33 @@ CREATE DATABASE "has_many_blogs" WITH OWNER "has_many_user";
 
 \c has_many_blogs;
 
-DROP TABLE IF EXISTS "user";
---CREATE TABLE "user"
+DROP TABLE IF EXISTS "usesr";
+CREATE TABLE "users"
+(
+id serial PRIMARY KEY NOT NULL,
+username varchar(90) NOT NULL,
+first_name varchar(90),
+last_name varchar(90),
+created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+);
+
+DROP TABLE IF EXISTS "posts";
+CREATE TABLE "posts"
+(
+id serial PRIMARY KEY NOT NULL,
+title varchar(180),
+url varchar(510),
+content text,
+created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+);
+
+DROP TABLE IF EXISTS "comments";
+CREATE TABLE "comments"
+(
+id serial PRIMARY KEY NOT NULL,
+body varchar(510),
+created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+);
