9a9121ff0d7dd9b6675ab3122a86b47339133594
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-25 00:15:06 -1000

    finished up to problem 5

--- joins.sql
+++ joins.sql
@@ -1,7 +1,19 @@
+-- 1
 SELECT * FROM users;
+-- 2
 SELECT * FROM posts WHERE users_id = 100;
+-- 3
 SELECT posts.*, first_name, last_name
   FROM users
   INNER JOIN posts
   ON  posts.users_id = users.id
   WHERE users_id = 200;
+-- 4
+SELECT posts.*, users.username
+  FROM users
+  INNER JOIN posts
+  ON posts.users_id = users.id
+  WHERE users.first_name = 'Norene' AND users.last_name = 'Schmitt';
+-- 5
+SELECT username FROM users WHERE created_at > '01/01/2015';
+-- 6
