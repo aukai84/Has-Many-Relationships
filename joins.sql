1cf810f180caf3cfa929b8198d6e5094ec179eab
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-25 18:37:21 -1000

    finished question 13

--- joins.sql
+++ joins.sql
@@ -45,3 +45,29 @@ SELECT comments.*, posts.title AS post_title, posts.url AS post_url, comments.bo
   INNER JOIN comments
   ON comments.posts_id = posts.id
   WHERE comments.body LIKE '%USB%';
+-- 11
+SELECT posts.title AS post_title, users.first_name, users.last_name,comments.body AS comment_body
+  FROM users
+  INNER JOIN posts
+  ON posts.users_id = users.id
+  INNER JOIN comments
+  ON comments.posts_id = posts.id
+  WHERE comments.body LIKE '%matrix%';
+-- 12
+SELECT users.first_name, users.last_name, comments.body AS comment_body
+  FROM users
+  INNER JOIN comments
+  ON comments.users_id = users.id
+  INNER JOIN posts
+  ON posts.id = comments.posts_id
+  WHERE comments.body LIKE '%SSL%' AND posts.content LIKE '%dolorum%';
+-- 13
+SELECT users.first_name AS post_author_first_name, users.last_name AS post_author_last_name, posts.title AS post_title, users.username AS comment_author_username, comments.body AS comment_body
+  FROM users
+  INNER JOIN posts
+  ON users.id = posts.users_id
+  INNER JOIN comments
+  ON posts.id = comments.posts_id
+  WHERE (comments.body LIKE '%SSL%' OR comments.body LIKE '%firewall%') AND posts.content LIKE '%nemo%';
+
+
